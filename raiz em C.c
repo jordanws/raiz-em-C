39c307a558f3957fd7458175816fa3ed26e1d518
Jowi <93743976+jordanws@users.noreply.github.com>
2024-01-01 23:15:21 -0300

    arquivo em .c para a calculo de raíz

---
+++ raiz em C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main(int argc, char *argv[])
+{
+  int numero = 8;// <-- colocar o numero que quer calcular
+ 
+  printf("A raiz de %d é %f", numero,
+    sqrt(numero));
+
+  return 0;
+}
